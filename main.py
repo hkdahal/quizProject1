2d504664b855acd386a85ce2d7ec2acf4f146cd6
Hari Dahal <hxd1478@rit.edu>
2016-03-21 23:16:55 -0400

    Gives a result message with score with a grade.

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ def loadQuiz():
                 # if answer is just one word
                 ans[mainKey] = lineList[1]
 
-def check(user_answer, qu, count):
+def check(user_answer, qu, count, correct):
     """
     Checks if the given answer matches with the correct one.
     Recursive if wrong answer is given.
@@ -44,12 +44,13 @@ def check(user_answer, qu, count):
     :param user_answer: user's input answer
     :param qu: key or question number
     :param count: count of how many times a user inputs an answer
-    :return: doesn't return anything, just prints or recurse appropriately
+    :return: count of correct answers
     """
     correct_answer = ans[qu].strip().lower()
     count += 1
     if len(user_answer) is len(correct_answer) and user_answer in \
             correct_answer:
+            correct += 1
             print("Correct answer\n")
     else:
         if (count is 3):  # correct answer revealed after 3 wrong answers
@@ -60,7 +61,52 @@ def check(user_answer, qu, count):
         while len(user_answer) is 0:
             # making sure user gives an answer not just an empty lin (an ENTER)
             user_answer = input("Enter answer: ")
-        check(user_answer, qu, count)
+        check(user_answer, qu, count, correct)
+
+    return correct
+
+
+def doTheQuiz(totalQuestions, num_questions):
+    """
+    The function that does the quiz taking in:
+    :param totalQuestions: the total questions number
+    :param num_questions: the number of questions user wants
+    :return: number (count) of correct answers
+    """
+    #making sure user gives a number greater than 0 and less than total questions
+    while (num_questions > totalQuestions or num_questions <= 0):
+        num_questions = int(input("Enter how many questions? "))
+
+    # a list holding randomly generated numbers
+    num_random = random.sample(range(1, totalQuestions+1), num_questions)
+    correct = 0
+    #print(num_random)
+    for qu in num_random:   # doing action as per the generated random numbers
+        user_answer = input(ques[qu] + "\nAnswer: ")
+        while len(user_answer) is 0:
+            # making sure user gives an answer not just an empty lin (an ENTER)
+            user_answer = input("Enter answer: ")
+        tryCount = 0
+        correct = check(user_answer, qu, tryCount, correct)
+    return correct
+
+def assignGrade(percent):
+    """
+    Assigning grade as per the percent scored for correct answers.
+    :param percent: percentage of correct answers out of total asked questions
+    :return: a grade (string)
+    """
+    if percent >= 90:
+        return "A"
+    elif percent >= 80:
+        return "B"
+    elif percent >= 70:
+        return  "C"
+    elif percent >= 60:
+        return "D"
+    else:
+        return "F"
+
 
 def main():
     """
@@ -72,23 +118,17 @@ def main():
 
     loadQuiz()  #loading quiz
     totalQuestions = len(ques)
-    num_questions = int(input("How many questions? (less than " + str(
-        totalQuestions) + "): "))
-    #making sure user gives a number greater than 0 and less than total questions
-    while (num_questions > totalQuestions or num_questions <= 0):
-        num_questions = int(input("Enter how many questions? "))
+    num_questions = 3 #int(input("How many questions? (less than " + str(
+        #totalQuestions) + "): "))
+    correct = doTheQuiz(totalQuestions, num_questions)
 
-    # a list holding randomly generated numbers
-    num_random = random.sample(range(1, totalQuestions+1), num_questions)
+    # calculating score percentage
+    percent = correct/num_questions * 100
 
-    #print(num_random)
-    for qu in num_random:   # doing action as per the generated random numbers
-        user_answer = input(ques[qu] + "\nAnswer: ")
-        while len(user_answer) is 0:
-            # making sure user gives an answer not just an empty lin (an ENTER)
-            user_answer = input("Enter answer: ")
-        tryCount = 0
-        check(user_answer, qu, tryCount)
+    # printing the result
+    print("You scored: " + str(percent) + " with grade: " + assignGrade(
+        percent))
+
 
 if __name__ == '__main__':
     main()
